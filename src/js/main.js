0f1e7d2c6756563df61c76315740b8d4ec9a937b
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-02 22:31:06 +0500

    added ui list all country watched

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 import "../css/main.css";
 
 import request from "./request";
+import { createCountries } from "./updataUI";
 
 const API = "https://restcountries.com/v3.1/all";
 
 request(API)
   .then((data) => {
-    console.log(data);
+    createCountries(data);
   })
+  .then(() => {})
   .catch((err) => {
     alert(err.message);
   });
---
+++ src/js/updataUI.js
@@ -0,0 +1,40 @@
+const cardsEl = document.querySelector(".cards");
+
+export const createCountries = (countries) => {
+  countries.forEach((country) => {
+    const commonName = country.name.common;
+    const population = country.population;
+    const region = country.region;
+    const capital = country.capital ? country.capital[0] : "No Capital";
+    const flag = country.flags.svg;
+
+    // li
+    const li = document.createElement("li");
+    li.classList.add("cards__item");
+    li.innerHTML = `
+      <a href="./about.html">
+        <img
+          src=${flag}
+          alt="germany-flag"
+          width="267"
+          height="160"
+        />
+        <div class="cards__item-inner">
+          <h3 class="cards__title">${commonName}</h3>
+          <p class="population">
+            Population: <span>${population}</span>
+          </p>
+          <p class="region">
+            Region: <span>${region}</span>
+          </p>
+          <p class="capital">
+            Capital: <span>${capital}</span>
+          </p>
+        </div>
+      </a>;
+    `;
+
+    // ul
+    cardsEl.appendChild(li);
+  });
+};
